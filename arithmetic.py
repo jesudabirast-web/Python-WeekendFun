716f2d0989c07d1d43e14f0090842d128d7b3be7
jesudabirast-web <jesudabira681@gmail.com>
2025-11-19 08:05:56 +0100

    in process

---
+++ arithmetic.py
@@ -0,0 +1,19 @@
+#prompt user to input numbers(integers)
+#sum up the integers inputed from the user
+#calculate the average and input of user's numbers 
+#for loop because the scripts to have a loop that inputs four integers
+
+
+
+number_one = int(input("Enter the first integer: "))
+number_two = int(input("Enter the second integer: "))
+number_three = int(input("Enter the third integer: "))
+number_four = int(input("Enter a fourth integer: "))
+
+sum = number_one + number_two + number_three + number_four
+average = sum / 4
+product = number_one * number_two * number_three * number_four
+
+count < 4
+for 
+
