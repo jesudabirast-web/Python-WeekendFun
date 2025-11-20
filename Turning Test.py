f28bdf0770e2f0aaae74d1965d42b7807e7f42a5
jesudabirast-web <jesudabira681@gmail.com>
2025-11-20 08:06:28 +0100

    in progress

---
+++ Turning Test.py
@@ -0,0 +1,8 @@
+problem = input("What is the problem?: ")
+question_1 = input("Have you experienced this problem before (yes or no)?: ")
+
+if question_1 == 0:
+	print("Well you have it again lol")
+
+if question_1 != 0:
+	print("Well...you have it now.")
---
+++ palindromes.py
@@ -0,0 +1,16 @@
+#prompt the user for a 5 digit integer
+#make it count 
+
+
+
+
+digit_1 = int(input("enter a first integer: "))
+digit_2 = int(input("enter a second integer: "))
+digit_3 = int(input("enter a third integer: "))
+digit_4 = int(input("enter a fourth integer: "))
+digit_5 = int(input("enter a fifth integer: "))
+
+count = 0
+
+while count < 0:
+	
